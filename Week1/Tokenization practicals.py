a5ea418e9aa9e5a235d7edafa2cca456d967c39c
samikshaps <pssamiksha54@gmail.com>
2025-05-25 12:42:53 +0530

    Add files via upload

--- Week1/Tokenization practicals.py
+++ Week1/Tokenization practicals.py
@@ -1,1 +1,28 @@
-#
+import nltk
+
+# Download punkt tokenizer (only needs to be run once)
+nltk.download('punkt')
+
+# Text to tokenize
+corpus = """My name is Samiksha.
+I'm learning this for SOC!"""
+
+# Import tokenizers
+from nltk.tokenize import sent_tokenize, word_tokenize
+
+# Sentence tokenization
+documents = sent_tokenize(corpus)
+print("Sentences:", documents)
+
+# Word tokenization (whole paragraph)
+print("Words (whole corpus):", word_tokenize(corpus))
+
+# Word tokenization per sentence
+for sentence in documents:
+    print("Words in sentence:", word_tokenize(sentence))
+
+from nltk.tokenize import wordpunct_tokenize
+print(wordpunct_tokenize(corpus))
+
+
+
